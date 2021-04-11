0fe19f96ccd47f6b9cc63c9de26009181ed9603c
PierreLvl <pierre.louvel@protonmail.com>
2021-04-11 14:01:50 +0800

    migration/deployment file creation

---
+++ migrations/2_deploy_test_env.js
@@ -0,0 +1,76 @@
+const Staking = artifacts.require("Staking");
+const Reward = artifacts.require("Reward");
+const TokenA = artifacts.require("TokenA");
+const TokenB = artifacts.require("TokenB");
+
+const StakingRewardMock = artifacts.require("StakingRewardMock");
+const UniswapV2RouterMock = artifacts.require("UniswapV2RouterMock");
+
+
+
+module.exports = (deployer, network, [owner]) => deployer
+  .then(() => deployStaking(deployer))
+  .then(() => deployReward(deployer))
+  .then(() => deployTokenA(deployer))
+  .then(() => deployTokenB(deployer))
+  .then(() => deployStakingReward(deployer))
+  .then(() => deployUniV2Router(deployer))
+  .then(() => transferRewardToStakingReward(owner))
+  .then(() => transferStakingToUniV2Router(owner))
+  .then(() => transferTokenAToUniV2Router(owner))
+  .then(() => transferTokenBToUniV2Router(owner));
+
+function deployStaking(deployer) {
+  return deployer.deploy(Staking, "UniV2-LP SDT-xSDT", "SDT-xSDT");
+}
+function deployReward(deployer) {
+  return deployer.deploy(Reward, "MUST Token", "MUST");
+}
+function deployTokenA(deployer) {
+  return deployer.deploy(TokenA, "Stake DAO Token", "SDT");
+}
+function deployTokenB(deployer) {
+    return deployer.deploy(TokenB, "Stake DAO Governance Token", "xSDT");
+  }
+
+async function deployStakingReward(deployer) {
+  const staking = (await Staking.deployed());
+  const reward = (await Reward.deployed());
+  return deployer.deploy(StakingRewardMock, staking.address, reward.address);
+}
+
+async function deployUniV2Router(deployer) {
+    const staking = (await Staking.deployed());
+    return deployer.deploy(UniswapV2RouterMock, staking.address);
+  }
+
+
+async function transferRewardToStakingReward(owner) {
+    const reward = (await Reward.deployed());
+    const stakingRewardMock = (await StakingRewardMock.deployed());
+    var amountToTransfer = web3.utils.fromWei(await reward.balanceOf(owner));
+    return reward.transfer(stakingRewardMock.address, web3.utils.toWei(amountToTransfer.toString()));
+}
+
+async function transferStakingToUniV2Router(owner) {
+    const staking = (await Staking.deployed());
+    const uniswapV2RouterMock = (await UniswapV2RouterMock.deployed());
+    var ownerBalance = web3.utils.fromWei(await staking.balanceOf(owner));
+    var amountToTransfer = ownerBalance * 0.9999;
+    return staking.transfer(uniswapV2RouterMock.address, web3.utils.toWei(amountToTransfer.toString()));
+}
+
+async function transferTokenAToUniV2Router(owner) {
+    const tokenA = (await TokenA.deployed());
+    const uniswapV2RouterMock = (await UniswapV2RouterMock.deployed());
+    var amountToTransfer = web3.utils.fromWei(await tokenA.balanceOf(owner));
+    return tokenA.transfer(uniswapV2RouterMock.address, web3.utils.toWei(amountToTransfer.toString()));
+}
+
+async function transferTokenBToUniV2Router(owner) {
+    const tokenB = (await TokenB.deployed());
+    const uniswapV2RouterMock = (await UniswapV2RouterMock.deployed());
+    var amountToTransfer = web3.utils.fromWei(await tokenB.balanceOf(owner));
+    return tokenB.transfer(uniswapV2RouterMock.address, web3.utils.toWei(amountToTransfer.toString()));
+}
+
---
+++ migrations/3_deploy_contracts.js
@@ -0,0 +1,94 @@
+
+/**
+ *  Mock Contracts
+ */
+const Staking = artifacts.require("Staking");
+const Reward = artifacts.require("Reward");
+const TokenA = artifacts.require("TokenA");
+const TokenB = artifacts.require("TokenB");
+const StakingRewardMock = artifacts.require("StakingRewardMock");
+const UniswapV2RouterMock = artifacts.require("UniswapV2RouterMock");
+
+/**
+ *  Contracts Under Tests
+ */
+const UniV2Optimizer = artifacts.require("UniV2Optimizer");
+
+
+module.exports = (deployer, network, [owner]) => deployer
+  .then(() => deployUniV2Optimizer(deployer))
+  .then(() => displaySummary(owner));
+
+
+async function deployUniV2Optimizer(deployer) {
+  const tokenA = (await TokenA.deployed());
+  const tokenB = (await TokenB.deployed());
+  const staking = (await Staking.deployed());
+  const reward = (await Reward.deployed());
+  const stakingRewardMock = (await StakingRewardMock.deployed());
+  const uniswapV2RouterMock = (await UniswapV2RouterMock.deployed());
+  return deployer.deploy(
+      UniV2Optimizer,
+      tokenA.address,
+      tokenB.address,
+      staking.address,
+      reward.address,
+      stakingRewardMock.address,
+      uniswapV2RouterMock.address
+    );
+}
+
+async function displaySummary(owner) {
+  const staking = (await Staking.deployed());
+  const reward = (await Reward.deployed());
+  const tokenA = (await TokenA.deployed());
+  const tokenB = (await TokenB.deployed());
+  const stakingRewardMock = (await StakingRewardMock.deployed());
+  const uniswapV2RouterMock = (await UniswapV2RouterMock.deployed());
+  const uniV2Optimizer = (await UniV2Optimizer.deployed());
+
+
+  console.log(
+    `===================================================
+
+    Deployed Contracts Addresses :
+
+        > Token Staking :           ${staking.address}
+        > Token Reward :            ${reward.address}
+        > Token A :                 ${tokenA.address}
+        > Token B :                 ${tokenB.address}
+        > Staking Reward Pool :     ${stakingRewardMock.address}
+        > UniV2 Router :            ${uniswapV2RouterMock.address}
+        > UniV2Optimizer :          ${uniV2Optimizer.address}
+
+    ===================================================
+
+    Balances:
+
+        > Owner :
+            Token Staking :     ${web3.utils.fromWei(await staking.balanceOf(owner))}
+            Token Reward :      ${web3.utils.fromWei(await reward.balanceOf(owner))}
+            Token A :           ${web3.utils.fromWei(await tokenA.balanceOf(owner))}
+            Token B :           ${web3.utils.fromWei(await tokenB.balanceOf(owner))}
+
+        > Staking Reward Pool :
+            Token Staking :     ${web3.utils.fromWei(await staking.balanceOf(stakingRewardMock.address))}
+            Token Reward :      ${web3.utils.fromWei(await reward.balanceOf(stakingRewardMock.address))}
+            Token A :           ${web3.utils.fromWei(await tokenA.balanceOf(stakingRewardMock.address))}
+            Token B :           ${web3.utils.fromWei(await tokenB.balanceOf(stakingRewardMock.address))}
+
+        > UniV2 Router :    
+            Token Staking :     ${web3.utils.fromWei(await staking.balanceOf(uniswapV2RouterMock.address))}
+            Token Reward :      ${web3.utils.fromWei(await reward.balanceOf(uniswapV2RouterMock.address))}
+            Token A :           ${web3.utils.fromWei(await tokenA.balanceOf(uniswapV2RouterMock.address))}
+            Token B :           ${web3.utils.fromWei(await tokenB.balanceOf(uniswapV2RouterMock.address))}
+
+        > UniV2Optimizer :  
+            Token Staking :     ${web3.utils.fromWei(await staking.balanceOf(uniV2Optimizer.address))}
+            Token Reward :      ${web3.utils.fromWei(await reward.balanceOf(uniV2Optimizer.address))}
+            Token A :           ${web3.utils.fromWei(await tokenA.balanceOf(uniV2Optimizer.address))}
+            Token B :           ${web3.utils.fromWei(await tokenB.balanceOf(uniV2Optimizer.address))}                               
+
+
+    ===================================================`);
+}
