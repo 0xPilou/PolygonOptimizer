ac84ea202cb2ea4f2f6ad654e8485736a5613f4f
PierreLvl <pierre.louvel@protonmail.com>
2021-04-18 23:28:37 +0800

    update test files to cover erc20 recovery function

--- test/1_test_UniV2Optimizer.js
+++ test/1_test_UniV2Optimizer.js
@@ -12,6 +12,7 @@ const Staking = artifacts.require("Staking");
 const Reward = artifacts.require("Reward");
 const TokenA = artifacts.require("TokenA");
 const TokenB = artifacts.require("TokenB");
+const TokenC = artifacts.require("TokenC");
 const StakingRewardMock = artifacts.require("StakingRewardMock");
 const UniswapV2RouterMock = artifacts.require("UniswapV2RouterMock");
 
@@ -26,6 +27,9 @@ contract("UniV2Optimizer Unit Tests", () => {
     let uniV2Optimizer = null;
     let staking = null;
     let reward = null;
+    let tokenA = null;
+    let tokenB = null;
+    let tokenC = null;
     let stakingReward = null;
     let uniV2Router = null;
     let accounts = null;
@@ -35,6 +39,9 @@ contract("UniV2Optimizer Unit Tests", () => {
         uniV2Optimizer = await UniV2Optimizer.deployed();
         staking = await Staking.deployed();
         reward = await Reward.deployed();
+        tokenA = await TokenA.deployed();
+        tokenB = await TokenB.deployed();
+        tokenC = await TokenC.deployed();
         stakingReward = await StakingRewardMock.deployed();
         uniV2Router = await UniswapV2RouterMock.deployed();
     });
@@ -56,6 +63,16 @@ contract("UniV2Optimizer Unit Tests", () => {
         );
     });
   
+    
+    it("should compound the Reward into Staking", async () => {
+       
+        const poolLpBal_before = web3.utils.fromWei(await staking.balanceOf(stakingReward.address));
+
+        await uniV2Optimizer.harvest();
+        const poolLpBal_after = web3.utils.fromWei(await staking.balanceOf(stakingReward.address));
+        
+        assert.equal(poolLpBal_before < poolLpBal_after, true);
+    });
     it("should withdraw 10 LP token from the Staking Reward Pool", async () => {
         const amountToWithdraw = 10;
         const weiAmountToWithdraw = web3.utils.toWei(amountToWithdraw.toString());
@@ -67,16 +84,39 @@ contract("UniV2Optimizer Unit Tests", () => {
 
         const userLpBal_after = web3.utils.fromWei(await staking.balanceOf(accounts[0]));
         const poolLpBal_after = web3.utils.fromWei(await staking.balanceOf(stakingReward.address));
-
-        assert.equal(userLpBal_after, 10);
-        assert.equal(poolLpBal_after, 0);
-        
+       
         assert.equal(userLpBal_after - userLpBal_before, amountToWithdraw);
         assert.equal(poolLpBal_before - amountToWithdraw, poolLpBal_after);
-    });
-  // it("should ...", async () => {
 
-  // });
+    });
+    it("should withdraw all Staking and Reward tokens form the Staking Reward Pool", async () => {
+
+        const userLpBal_before = web3.utils.fromWei(await staking.balanceOf(accounts[0]));
+        const poolLpBal_before = web3.utils.fromWei(await staking.balanceOf(stakingReward.address));
+
+        await uniV2Optimizer.exitAvalanche();
+
+        const userLpBal_after = web3.utils.fromWei(await staking.balanceOf(accounts[0]));
+        const poolLpBal_after = web3.utils.fromWei(await staking.balanceOf(stakingReward.address));
+
+        assert.equal(userLpBal_after > userLpBal_before, true);
+        assert.equal(poolLpBal_before > poolLpBal_after, true);
+        
+    });
+
+    it("should recover a lost TokenC from the contract", async () => { 
+        const poolTokenCBal_before = web3.utils.fromWei(await tokenC.balanceOf(uniV2Optimizer.address));
+        const userTokenCBal_before = web3.utils.fromWei(await tokenC.balanceOf(accounts[0]));
+
+        await uniV2Optimizer.recoverERC20(tokenC.address);
+
+        const poolTokenCBal_after = web3.utils.fromWei(await tokenC.balanceOf(uniV2Optimizer.address));
+        const userTokenCBal_after = web3.utils.fromWei(await tokenC.balanceOf(accounts[0]));
+
+        assert.equal(poolTokenCBal_before > poolTokenCBal_after, true);
+        assert.equal(userTokenCBal_before < userTokenCBal_after, true);   
+        
+    });
 
 });
 
